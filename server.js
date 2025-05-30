b94f3d6117b0ecd400ad9791008b7192162393c9
jamshid98 <jamshid98991213@gmail.com>
2025-05-31 02:18:46 +0900

    Describe your changes here

--- server.js
+++ server.js
@@ -4,24 +4,30 @@ const app = express();
 const http = require("http");  //core modulemiz
 
 // 1: Kirish code 
-app.use(express.static("public")); //bundan cssga qurgan narslarni olib kelamiz
-app.use(express.json()); //kirib kelayotgan atani object holatiga ogirib beradi
-app.use(express.urlencoded({extended: true})); //htmlda formrequest bor(tradion) htmldan qilingan postni kiritib beradi
+app.use(express.static("public")); 
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true})); 
 
-// 2:Session code
 // 3:Views code
-app.set("views", "views"); //BssRda backda front end yaasamiz yani htmlga yboramiz
+app.set("views", "views"); 
 app.set("view engine", "ejs");
 
-// 4: Routing code rooterlarga moljallangan
+// 4: Routing code
 app.get("/hello", function (req, res){
  res.end(`<h1>HELLO WORLD by JAMSHID rasulov</h1>`);
 });
 app.get("/gift", function (req, res){
  res.end(`<h1>siz sovgalar bolimidasiz</h1>`);
 });
+
 const server = http.createServer(app);
-let PORT = 3005;
-server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}`);
-}); 
+let PORT = 3007;
+
+// Faqatgina bevosita ishga tushirilganda serverni tingla
+if (require.main === module) {
+  server.listen(PORT, function () {
+    console.log(`The server is running successfully on port: ${PORT}`);
+  });
+}
+
+module.exports = app;
---
+++ task.js
@@ -0,0 +1,10 @@
+console.log('train task ishga tushdi!');
+
+// Bu yerda train.js uchun boshqa ishlar qilinadi
+
+// boshqa train task kodi
+
+
+// b task
+
+// a task
